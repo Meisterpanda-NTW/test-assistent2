9355fc493d216fa47f124d36ae147d3767c529b6
Meisterpanda <lukassojer2011@gmail.com>
2026-07-30 22:06:51 +0200

    Update geil.py

--- geil.py
+++ geil.py
@@ -12,7 +12,7 @@ import io
 
 
 st.set_page_config(page_title="Garmin KI Assistent", page_icon="🤖")
-st.title("🤖 Garmin")
+st.title("🤖 Garmin 🤖 vertrau bruder")
 
 # HIER DEINE EIGENEN GOOGLE GEMINI SCHLÜSSEL EINTRAGEN:
 API_KEYS = [
